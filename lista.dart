ed9e3158815dd845099cda6892bfa02fd036238b
ArielRodriguez07 <a.22308051280706@cbtis128.edu.mx>
2025-02-13 01:53:52 +0000

    publicar mapa1

---
+++ lista.dart
@@ -0,0 +1,14 @@
+ 
+
+void main() { 
+  print("Ariel Rodriguez Ceniceros 22308051280706 6J"); 
+   List<int> numeros = [10,20,30];
+   print("Lista de numeros enteros: ");
+   print(numeros);
+   print("Primer elemento: ${numeros[0]}");
+   print("Segundo elemento: ${numeros[1]}");
+  print("Tercer elemento: ${numeros[2]}");
+  for (int i = 0; i < numeros.length; i++) {
+    print("Elemento en el índice $i: ${numeros[i]}");
+  }
+}  
